38bd4cbde57496c3f6ecf03e20b0009d182a9f7e
felipewmartins <felipewmartins@gmail.com>
2014-08-03 11:08:23 -0300

    Developing the connection methods

---
+++ src/main/java/org/fmartins/jwhats/registration/CodeRequest.java
@@ -0,0 +1,10 @@
+package org.fmartins.jwhats.registration;
+
+public class CodeRequest {
+  private String p_in;
+  private String url ="v.whatsapp.net/v2/code";
+
+  public void getCode(String cc, String in){
+    
+  }
+}
--- src/main/java/org/fmartins/jwhats/registration/RegisterManager.java
+++ src/main/java/org/fmartins/jwhats/registration/RegisterManager.java
@@ -1,17 +1,26 @@
 package org.fmartins.jwhats.registration;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class RegisterManager {
 
-  private final String url = "v.whatsapp.net/v2/register?";
+  private final String url = "v.whatsapp.net/v2/register";
+  private String charset = "UTF-8";
   private String pr_in = "in";
   private String cc;
+  private CodeRequest code;
   
-  public void registerNumber(){
-    
+  public void registerNumber(String cc, String in){
+    try {
+      URLConnection connection = new URL(url+"?").openConnection();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
